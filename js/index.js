edf56eb85175d0ef80d4b8fac5c3dffb524cf64c
Anastasia Chen <anastasia@Anastasias-MacBook-Air.local>
2020-12-10 13:57:46 +1100

    add form validation

---
+++ js/index.js
@@ -0,0 +1,70 @@
+// Select the Task Form
+const taskForm = document.querySelector('#taskForm');
+
+// Add an 'onsubmit' event listener
+taskForm.addEventListener('submit', (event) => {
+    // Prevent default action
+    event.preventDefault();
+
+    // Select the inputs
+    const taskName = document.querySelector('#taskName');
+    const taskDescription = document.querySelector('#taskDescription');
+    const taskAssignedTo = document.querySelector('#taskAssignedTo');
+    const taskDueDate = document.querySelector('#taskDueDate');
+    const taskStatus = document.querySelector('#taskStatus');
+    const errorMessage = document.querySelector('#alertMessage');
+
+    // Get the values of the inputs
+    const name = taskName.value;
+    const description = taskDescription.value;
+    const assignedTo = taskAssignedTo.value;
+    const dueDate = taskDueDate.value;
+    const status = taskStatus.value;
+
+    console.log(name);
+    console.log(description);
+    console.log(dueDate);
+    console.log(assignedTo);
+    console.log(status);
+    //console.log(priority);
+
+    //name validation
+    if (!validFormFieldInput(name)) {
+        errorMessage.innerHTML = 'Invalid name input';
+        errorMessage.style.display = 'block';
+        console.log('validating name if block');
+    
+    //description validation
+    } else if (!validFormFieldInput(description)) {
+        errorMessage.innerHTML = 'Invalid description input';
+        errorMessage.style.display = 'block';
+        console.log('validating description if block');
+
+    //date validation
+}   else if (dueDate == 0) {
+    errorMessage.innerHTML = 'Please select Due Date';
+    errorMessage.style.display = 'block';
+    console.log('validating due date if block');
+
+    //assigned to validation
+    } else if (assignedTo == 0) {
+        errorMessage.innerHTML = 'Please select Assigned to';
+        errorMessage.style.display = 'block';
+        console.log('validating assigned to if block');
+    
+    //dropdown status validation
+    } else if (status == 0){ 
+        errorMessage.innerHTML = 'Please select Status';
+        errorMessage.style.display = 'block';
+        console.log('validating status if block');
+    
+    } else {
+        errorMessage.style.display = 'none';
+        console.log('status else block');
+    };
+
+});
+
+function validFormFieldInput(data){
+    return data !== null && data !== '';
+}
